41334ed3f42f3629dc38591cd8d0fd7b1bb6fb25
Gabriele KURAITE <gkuraite@e2r7p9.42.fr>
2018-06-19 15:38:39 +0200

    va_start test

--- va_start1.c
+++ va_start1.c
@@ -6,20 +6,41 @@
 /*   By: gkuraite <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/18 15:22:46 by gkuraite          #+#    #+#             */
-/*   Updated: 2018/06/18 15:34:00 by gkuraite         ###   ########.fr       */
+/*   Updated: 2018/06/19 15:38:02 by gkuraite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include  <stdio.h>
 #include <stdarg.h>
 
-void	yas_bitch(int numbers, ...)
+int	yas_bitch(int numbers, ...)
 {
-	va_list
+	va_list		list;
+	int		total;
+
+	total = 0;
+	va_start(list, numbers);
+	int		count = 0;
+	while (count < numbers)
+	{
+		total = total + va_arg(list, int);
+		count++;
+	}
+	va_end(list);
+	return (total);
 }
 
 int		main(void)
 {
-	yas_bitch();
+	int		v1, v2, v3, v4;
+	int		result;
+
+	v1 = 1;
+	v2 = 2;
+	v3 = 3;
+	v4 = 4;
+
+	result = yas_bitch(4, v1, v2, v3, v4);
+	printf("%d\n", result);
 	return (0);
 }
